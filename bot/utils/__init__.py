09e45c0f1048202e97db1837e30f29fb199b988d
Chloe Winter <git@eolhc.dev>
2025-11-17 21:24:04 +0000

    move `upload_map_to_archive` to its own file

--- bot/utils/__init__.py
+++ bot/utils/__init__.py
@@ -21,6 +21,7 @@ from diplomacy.persistence.unit import UnitType
 from .logging import log_command, log_command_no_ctx
 from .send_message import send_message_and_file
 from .orders import get_orders, get_filtered_orders
+from .map_archive import upload_map_to_archive
 
 logger = logging.getLogger(__name__)
 
@@ -237,41 +238,3 @@ def get_value_from_timestamp(timestamp: str) -> int | None:
 
     return None
 
-async def upload_map_to_archive(ctx: commands.Context, server_id: int, board: Board, map: str, turn: tuple[str, phase] | None = None) -> None:
-    if "maps_sas_token" not in os.environ:
-        return
-    if turn is None:
-        turnstr = f"{(board.year + board.year_offset) % 100}{board.phase.shortname}"
-    else:
-        turnstr = f"{int(turn[0]) % 100}{turn[1].shortname}"
-    url = None
-    with open("gamelist.tsv", "r") as gamefile:
-        for server in gamefile:
-            server_info = server.strip().split("\t")
-            if str(server_id) == server_info[0]:
-                url = f"{os.environ['maps_url']}/{server_info[1]}/{server_info[2]}/{turnstr}m.png{os.environ['maps_sas_token']}"
-                break
-    if url is None:
-        return
-    png_map, _ = await svg_to_png(map, url)
-    p = await asyncio.create_subprocess_shell(
-        f'azcopy copy "{url}" --from-to PipeBlob --content-type image/png',
-        stdout=PIPE,
-        stdin=PIPE,
-        stderr=PIPE,
-    )
-    data, error = await p.communicate(input=png_map)
-    error = error.decode()
-    await send_message_and_file(
-        channel=ctx.channel,
-        title=f"Uploaded map to archive",
-    )
-    log_command(
-        logger,
-        ctx,
-        message=(
-            f"Map uploading failed: {error}"
-            if len(error) > 0
-            else "Uploaded map to archive"
-        ),
-    )
---
+++ bot/utils/map_archive.py
@@ -0,0 +1,50 @@
+import asyncio
+import os
+
+from subprocess import PIPE
+from discord.ext import commands
+from diplomacy.adjudicator.utils import svg_to_png
+from diplomacy.persistence import phase
+from diplomacy.persistence.board import Board
+
+from bot.utils import log_command, send_message_and_file
+
+
+async def upload_map_to_archive(ctx: commands.Context, server_id: int, board: Board, map: str, turn: tuple[str, phase] | None = None) -> None:
+    if "maps_sas_token" not in os.environ:
+        return
+    if turn is None:
+        turnstr = f"{(board.year + board.year_offset) % 100}{board.phase.shortname}"
+    else:
+        turnstr = f"{int(turn[0]) % 100}{turn[1].shortname}"
+    url = None
+    with open("gamelist.tsv", "r") as gamefile:
+        for server in gamefile:
+            server_info = server.strip().split("\t")
+            if str(server_id) == server_info[0]:
+                url = f"{os.environ['maps_url']}/{server_info[1]}/{server_info[2]}/{turnstr}m.png{os.environ['maps_sas_token']}"
+                break
+    if url is None:
+        return
+    png_map, _ = await svg_to_png(map, url)
+    p = await asyncio.create_subprocess_shell(
+        f'azcopy copy "{url}" --from-to PipeBlob --content-type image/png',
+        stdout=PIPE,
+        stdin=PIPE,
+        stderr=PIPE,
+    )
+    data, error = await p.communicate(input=png_map)
+    error = error.decode()
+    await send_message_and_file(
+        channel=ctx.channel,
+        title=f"Uploaded map to archive",
+    )
+    log_command(
+        logger,
+        ctx,
+        message=(
+            f"Map uploading failed: {error}"
+            if len(error) > 0
+            else "Uploaded map to archive"
+        ),
+    )
